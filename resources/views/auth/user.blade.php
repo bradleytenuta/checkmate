a47f5c1e27db2c59060c57dab055ddf0a69cd751
BradBitt <bradleyt2009@hotmail.co.uk>
2020-02-03 16:05:30 +0000

    Added a user page with some information.

--- resources/views/auth/user.blade.php
+++ resources/views/auth/user.blade.php
@@ -1,7 +1,12 @@
 @extends ('layouts.main')
-
 <!-- Begining of the Section-->
 @section ('dynamic-main-content')
+
+<!-- Content Container -->
+<div class="content-container">
+    <!-- User Card -->
+    @include('components.user.page.infobox', ['user'=>$user])
+</div>
 
 @endsection
 <!-- End of the Section-->
