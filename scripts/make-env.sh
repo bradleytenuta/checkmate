66a6ed5dd5d51351311158678ef54499798aff05
BradBitt <bradleyt2009@hotmail.co.uk>
2020-03-03 09:56:03 +0000

    moved composer install into .env file.

--- scripts/make-env.sh
+++ scripts/make-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Creates the '.env' file and generates a key.
 cd ..
 cd src
+
+# Makes sure composer modules are up to date.
+composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scripts
+
+# Creates the '.env' file and generates a key.
 cp .env.example .env
 php artisan key:generate
