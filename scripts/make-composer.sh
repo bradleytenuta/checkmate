a98b4040ba0951dab70747bca0d1f83c915fa21a
BradBitt <bradleyt2009@hotmail.co.uk>
2020-03-10 14:50:59 +0000

    Now deletes the composer install file once composer is installed. Now automatically updates the port in the docker compose file to be 80. Updated Doc

--- scripts/make-composer.sh
+++ scripts/make-composer.sh
@@ -7,3 +7,6 @@ sudo apt install php7.3 php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7
 # Installs Composer
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+# Removes the Composer install file, as it's no longer needed.
+sudo rm composer-setup.php
