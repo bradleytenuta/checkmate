6ae0f2cb658ba2007eeacfc203ecc21cd5d70a80
BradBitt <bradleyt2009@hotmail.co.uk>
2020-03-10 13:55:20 +0000

    restored the artisan command. now trying to fix the version of composer that is installed. Maybe this different approach will do it.

--- scripts/make-composer.sh
+++ scripts/make-composer.sh
@@ -2,15 +2,8 @@
 
 # Installs dependencies. Including PHP.
 sudo apt update
-sudo apt install libapache2-mod-php7.3 php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7.3-xml php7.3-cli php7.3-zip
+sudo apt install php7.3 php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7.3-xml php7.3-cli php7.3-zip
 
 # Installs Composer
-sudo curl -sS https://getcomposer.org/installer
-
-# Adds composer to env var
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
-source ~/.bashrc
-
-# Checks Composer is installed correctly.
-composer -v
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
