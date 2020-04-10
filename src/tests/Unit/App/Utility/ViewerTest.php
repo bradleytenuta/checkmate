56c26de89ceac193483396cc503f662f0766fb20
BradBitt <bradleyt2009@hotmail.co.uk>
2020-04-10 16:06:00 +0100

    Added viewer unit tests

---
+++ src/tests/Unit/App/Utility/ViewerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit\App\Utility;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Utility\Viewer;
+
+class ViewerTest extends TestCase
+{
+    /**
+     * Tests that the string is formatted correctly.
+     *
+     * @return void
+     */
+    public function testFormatLine()
+    {
+        $before = "\t\r\n";
+        $after = "    ";
+        $this->assertTrue(Viewer::formatLine($before) == $after);
+    }
+
+    /**
+     * Tests that nothing happens to the string as it is already formatted.
+     *
+     * @return void
+     */
+    public function testFormatLineNoChange()
+    {
+        $before = "test";
+        $this->assertTrue(Viewer::formatLine($before) == $before);
+    }
+}
