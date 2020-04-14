6508755b704b9795e2c118b2cd5d0161935611a7
BradBitt <bradleyt2009@hotmail.co.uk>
2020-04-14 14:05:41 +0100

    Fixed dodgying class stying and used a minimal css approach to the line containers. Removed js to style the line containers.

--- src/public/js/pages/viewer.js
+++ src/public/js/pages/viewer.js
@@ -93,25 +93,9 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$(document).ready(function () {
-  adjustNumberContainerWidth();
-});
-/**
- * Adjusts the width of the number containers to match the width of the
- * last container.
- */
-
-window.adjustNumberContainerWidth = function adjustNumberContainerWidth() {
-  // Loops through all the number containers and
-  var maxWidth = $('.viewer-number-container').last().width(); // Sets the width of all the number containers
-
-  $('.viewer-number-container').width(maxWidth);
-};
 /**
  * This fucntion creates the open close slide used for mobile devices.
  */
-
-
 $('#viewer-left-container-menu-button').click(function () {
   // Gets the button.
   var button = $('#viewer-left-container-menu-button');
--- src/resources/js/pages/viewer.js
+++ src/resources/js/pages/viewer.js
@@ -1,19 +1,3 @@
-$(document).ready(function() {
-    adjustNumberContainerWidth();
-});
-
-/**
- * Adjusts the width of the number containers to match the width of the
- * last container.
- */
-window.adjustNumberContainerWidth = function adjustNumberContainerWidth() {
-    // Loops through all the number containers and
-    var maxWidth = $('.viewer-number-container').last().width();
-
-    // Sets the width of all the number containers
-    $('.viewer-number-container').width(maxWidth);
-}
-
 /**
  * This fucntion creates the open close slide used for mobile devices.
  */
